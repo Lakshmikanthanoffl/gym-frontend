6f2a67b452a987165812bf07c795e22403d03bce
lakshmikanthan.babu <lakshmikanthan.babu@xtract.io>
2025-09-17 16:49:15 +0530

    added file changes

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ platformBrowser().bootstrapModule(AppModule, {
   .catch(err => console.error(err));
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
-      navigator.serviceWorker
-        .register("/sw.js")
+      navigator.serviceWorker.register('/assets/sw.js')
+
         .then((reg) => console.log("Service Worker registered:", reg.scope))
         .catch((err) => console.log("Service Worker registration failed:", err));
     });
